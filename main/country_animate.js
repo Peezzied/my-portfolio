c677fa63b484952828065bdf1ba106bb2b4efd27
BuildTools <unconfigured@null.spigotmc.org>
2024-01-09 19:14:40 +0800

    animating

--- main/country_animate.js
+++ main/country_animate.js
@@ -1,12 +1,21 @@
 const country = document.querySelector('.country')
-country.style.setProperty('--pos-r', 50 + 'px')
-country.addEventListener('mousedown', (e)=>{
+const radius = 0.1
+const x = 0
+const y = 0
+
+// defaults
+
+country.style.setProperty('--pos-r', radius + 'em')
+
+document.addEventListener('mousedown', (e)=>{
     let x = e.clientX - e.target.offsetLeft
     let y = e.clientY - e.target.offsetTop
     let pos = e.target.getBoundingClientRect();
     console.log(x, y, pos)
     e.target.style.setProperty('--pos-x', x + 'px')
     e.target.style.setProperty('--pos-y', y + 'px')
+
+    e.target.class
 })
 
 function getRandomColor() {
