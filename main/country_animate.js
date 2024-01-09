58f0c922a56903ff1f609cb741203e27090de39a
BuildTools <unconfigured@null.spigotmc.org>
2024-01-09 22:59:03 +0800

    color animation

--- main/country_animate.js
+++ main/country_animate.js
@@ -1,22 +1,55 @@
-const country = document.querySelector('.country')
-const radius = 0.1
+const country = document.querySelector('#country')
+const countryStyle = document.querySelector('.country')
+const root = document.documentElement
 const x = 0
 const y = 0
 
 // defaults
+const radius = 0.1
+const timing = 500;
+let currentAnimation;
+let color = 0;
+root.style.setProperty('--timing', timing + 'ms')
 
-country.style.setProperty('--pos-r', radius + 'em')
-
-document.addEventListener('mousedown', (e)=>{
+country.addEventListener('mousedown', (e)=>{
+    const colors = ['rgb(252, 209, 22)', 'rgb(0, 56, 168)', 'rgb(206, 17, 38)', 'rgb(255, 255, 255)']
     let x = e.clientX - e.target.offsetLeft
     let y = e.clientY - e.target.offsetTop
     let pos = e.target.getBoundingClientRect();
-    console.log(x, y, pos)
-    e.target.style.setProperty('--pos-x', x + 'px')
-    e.target.style.setProperty('--pos-y', y + 'px')
+    color = color % 4
+    //console.log(x, y)
+    
+    root.style.setProperty('--pos-x', x + 'px')
+    root.style.setProperty('--pos-y', y + 'px')
+    root.style.setProperty('--color', colors[color])
+    
+    const keyframes = [
+        {backgroundSize: '100% 100%'},
+        {backgroundSize: '4000% 4000%'}
+    ]
+    const animationProp = {
+        duration: timing,
+        easing: 'ease-in-out',
+        fill: 'forwards'
+    }
+    if (currentAnimation){
+        currentAnimation.cancel()
+    }
+    var animation = e.target.animate(keyframes, animationProp)
+    currentAnimation = animation
+    animation.onfinish = ()=>{
+        root.style.setProperty('--color-primary', root.style.getPropertyValue('--color'))
+        console.log(colors[color])
+        color++;
+    }
+    // e.target.classList.add('countryChange')
+    // setTimeout(()=>{
+    //     e.target.classList.remove('country')
+    // }, timing)
 
-    e.target.class
+
 })
+
 
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
